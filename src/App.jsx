a7c19a1128ad2a133000b273e5f051ba65914e65
Albert <albertbarsegyan6@gmail.com>
2021-08-09 23:49:50 +0400

    docs(store.js): make file for redux store

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <div>
+          <p>Counter :</p>
+        </div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button type="button">Decrement</button>
+        <button type="button">Increment</button>
       </header>
     </div>
   );
